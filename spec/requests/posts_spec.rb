3f4af7a2f7c23cc6e360529aa3eb2d7206ecc450
Gracerue <munyaradzi045@gmail.com>
2023-06-26 21:02:12 +0200

    Finish views integration

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,18 +1,18 @@
 RSpec.describe 'Posts', type: :request do
   describe 'GET #index' do
     it 'returns a successful response' do
-      get '/users/5/posts'
+      get '/users/645/posts'
       expect(response).to have_http_status(:success)
     end
 
     it 'renders the index template' do
-      get '/users/5/posts'
+      get '/users/645/posts'
       expect(response).to render_template(:index)
     end
 
     it 'includes correct placeholder text in the response body' do
-      get '/users/5/posts'
-      expect(response.body).to include('<h1>List of all posts</h1>')
+      get '/users/645/posts'
+      expect(response.body).to include('<h3>Honesty and Integrity</h3>')
     end
   end
 
@@ -20,18 +20,18 @@ RSpec.describe 'Posts', type: :request do
     let(:post) { create(:post) }
 
     it 'returns a successful response' do
-      get '/users/5/posts/5'
+      get '/users/645/posts/533'
       expect(response).to have_http_status(:success)
     end
 
     it 'renders the show template' do
-      get '/users/5/posts/5'
+      get '/users/645/posts/533'
       expect(response).to render_template(:show)
     end
 
     it 'includes correct placeholder text in the response body' do
-      get '/users/5/posts/5'
-      expect(response.body).to include('<h1>Posts for a given user ID</h1>')
+      get '/users/645/posts/533'
+      expect(response.body).to include('<h2>Morality by Thomas Heflord</h2>')
     end
   end
 end
